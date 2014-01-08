e4ec801a8af3c51766db1329a6595612f9cf2823
Jelani Abdus-Salaam <jelani0694@gmail.com>
2014-01-08 03:37:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jelanisite::Application.config.secret_key_base = 'c53d35a6e5441161a62e6b03465e6cd11436582ba7aa62b3a15e284dd012f2dad1ee936000b4ce0816657805d1e95e6ab44f45ae9ecb2bc44640e8c6ac4cc7ff'
